6d22d765715773d1df4ab904b0df07ff755fbacb
Student <example@example.com>
2019-06-26 11:28:16 -0700

    count syllables in a single word

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -1,14 +1,83 @@
-import { syllablesInWord } from './../src/backend-code';
+import { syllablesInWord, countVowels, countDiphthongs, countSilentVowels} from './../src/backend-code';
+
 
 describe ('SyllableTest', function(){
 
-  it('should return syllables in word', function(){
-    expect(syllablesInWord('word')).toEqual(3);
-  })
+  // it('should return syllables in word', function(){
+  //   expect(syllablesInWord('word')).toEqual(3);
+  // });
+  //
+  // // it('should return syllables in word the second', function(){
+  // //   expect(syllablesInWord('word')).toEqual(2);
+  // // })
+  //
+  // it('should return # of vowels in a word', function(){
+  //   expect(countVowels("Trevor")).toEqual(2);
+  // });
+  //
+  // it('should return # of vowels in a word2', function(){
+  //   expect(countVowels("TrEvor")).toEqual(2);
+  // });
+  // // it('should return the number of diphthongs', function(){
+  // //   expect(countDiphthongs("house")).toEqual(1);
+  // // })
+  // //
+  // // it('should return the number of diphthongs', function(){
+  // //   expect(countDiphthongs("roundhouse")).toEqual(2);
+  // // })
+  // it('should return the number of diphthongs', function(){
+  //   expect(countDiphthongs("boat")).toEqual(1);
+  // });
+  //
+  // it('should return the number of diphthongs', function(){
+  //   expect(countDiphthongs("year")).toEqual(1);
+  // });
+  //
+  // it('should return the number of silent vowels1', function(){
+  //   expect(countSilentVowels("house")).toEqual(1);
+  // });
+  //
+  // it('should return the number of silent vowels2', function(){
+  //   expect(countSilentVowels("neat")).toEqual(0);
+  // });
+  //
+  // it('should return the number of silent vowels3', function(){
+  //   expect(countSilentVowels("le")).toEqual(0);
+  // });
+  it('return syllables in a word 1', function(){
+    expect(syllablesInWord("house")).toEqual(1);
+  });
 
-  it('should return syllables in word', function(){
-    expect(syllablesInWord('word')).toEqual(3); 
-  })
+  it('return syllables in a word 2', function(){
+    expect(syllablesInWord("peanut")).toEqual(2);
+  });
 
+  it('return syllables in a word 3', function(){
+    expect(syllablesInWord("Trever")).toEqual(2);
+  });
 
-})
+  it('return syllables in a word 4', function(){
+    expect(syllablesInWord("Emerson")).toEqual(3);
+  });
+
+  it('return syllables in a word 5', function(){
+    expect(syllablesInWord("roundhouse")).toEqual(2);
+  });
+
+  it('return syllables in a word 6', function(){
+    expect(syllablesInWord("blackguard")).toEqual(2);
+  });
+
+  it('return syllables in a word 7', function(){
+    expect(syllablesInWord("tickle")).toEqual(2);
+  });
+
+  it('return syllables in a word 8', function(){
+    expect(syllablesInWord("outside")).toEqual(2);
+  });
+
+  it('return syllables in a word 9', function(){
+    expect(syllablesInWord("tyranosaurus")).toEqual(5);
+  });
+
+});
--- src/backend-code.js
+++ src/backend-code.js
@@ -1,3 +1,133 @@
 export function syllablesInWord (word) {
-  return 3;
+  debugger;
+  if (specialWords[word]){
+    return specialWords[word];
+  } else {
+    let vowelCount = countVowels(word);
+    if (vowelCount === 1){
+      return 1;
+    } else {
+      vowelCount -= countDiphthongs(word);
+      vowelCount -= countSilentVowels(word);
+      if (vowelCount < 1){
+        throw "somehow started with more than 1 vowel and returned less than 1 vowel";
+      }
+      return vowelCount;
+    }
+  }
 }
+
+let vowels = ['a','e','i','o','u','y'];
+let specialWords = {
+  "quiet" : 2,
+  "buick" : 2
+
+}
+//get vowels
+export function countVowels(word) {
+  let counter = 0;
+  for (let i =0; i<word.length; i++) {
+    if (vowels.includes(word.charAt(i).toLowerCase())) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+//takes in word, returns number of consecutive vowel pairs
+export function countDiphthongs(word){
+  // let diphThongs = ["ow", "oe", "oo", "ue", "ey", "ay", "oy", "oi", "au", "aw", "ou", "ea", "ei", "ie"];
+  let counter = 0;
+  for(let i = 0; i < word.length - 1; i++){
+    let a;
+    let char1 = word.charAt(i).toLowerCase();
+    if (vowels.includes(char1)){
+      a = char1;
+      if (a){
+        let b;
+        let char2 = word.charAt(i + 1).toLowerCase();
+        if (vowels.includes(char2)){
+          b = char2;
+          if (b){
+            if (a === "o" && b === "a" && i === (word.length - 2)){
+              counter--;
+            } else if (a === "y"){
+              //do nothing
+            } else {
+              counter++;
+            }
+          }
+        }
+      }
+    }
+  }
+  return counter;
+}
+
+//if last 2 == le and previous letter != vowel do nothing else  +1
+
+export function countSilentVowels(word) {
+  let counter = 0;
+  if (word.charAt(word.length-1) === 'e') {
+    if(word.substring(word.length -2, word.length) === 'le') {
+      if(vowels.includes(word.charAt(word.length-3))) {
+        counter++
+      } else {
+        //do nothing
+      }
+    } else {
+      counter++
+    }
+  }
+  return counter;
+}
+
+
+
+
+
+
+
+
+
+
+
+//subtract methods:
+//vowels -= yBeforeVowel(word)
+//y before vowel: coyote: return 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// vowelCount++
+//
+// if eindexOf == word.length
+//   vowelCount--
+//
+// cap a ble
+// a ble
+//yes
+//yellow
+//y before vowel === subtract 1
+// fum ble
+// tic kle
+//
+// name
+// tale
